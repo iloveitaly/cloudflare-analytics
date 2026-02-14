1045de7d63a534d0d3d0fe3a8418b5139d37e255
Michael Bianco <mike@mikebian.co>
2026-02-14 10:01:46 -0700

    build: remove script entrypoints and cleanup logging config
    Generated-by: aiautocommit

--- cloudflare_analytics/__init__.py
+++ cloudflare_analytics/__init__.py
@@ -5,13 +5,8 @@ API docs: https://developers.cloudflare.com/analytics/graphql-api/
 """
 
 import logging
-import os
 
 from .client import CloudflareAnalyticsClient, GraphQLResponse, get_analytics_client
-
-logging.basicConfig(
-    level=os.environ.get("LOG_LEVEL", "INFO").upper(),
-)
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +16,3 @@ __all__ = [
     "get_analytics_client",
 ]
 
-
-def main():
-    logger.info("Cloudflare Analytics Client - use as a library")
